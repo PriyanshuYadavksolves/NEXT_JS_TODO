375c2efddbe8b8889ee4b29bb26b7f11ae9a72b2
Priyanshu Yadav <priyanshu.yadav@ksolves.com>
2024-06-12 16:20:26 +0530

    add crawler feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // This header applies to all pages
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'index, follow',  // Ensures all pages can be indexed and followed
+            },
+          ],
+        },
+      ];
+    },
+    trailingSlash: true,  // Optional: Adds a trailing slash to URLs
+  };
+  
+  export default nextConfig;
+  
